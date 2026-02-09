233f481a3a6f5547d2861534e999bca4012d9f19
tarikhkhan <tarikhhussainkhan007@gmail.com>
2026-02-10 03:29:33 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
     cancelButtonText: "No",
   }).then((result) => {
     if (result.isConfirmed) {
-      document.querySelector("hbd.mp3").play();
+      document.querySelector(".song").play();
       animationTimeline();
     } else {
       animationTimeline();
